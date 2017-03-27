59de74c543da644673242eb8aec79703d8a3e768
mousewip <mousewip@gmail.com>
2017-03-27 21:13:53 +0700

    a

--- QLBH_BunifuUI/form/childFrmSetting.Designer.cs
+++ QLBH_BunifuUI/form/childFrmSetting.Designer.cs
@@ -33,6 +33,7 @@ namespace QLBH_BunifuUI.form
         {
             this.components = new System.ComponentModel.Container();
             this.childFrmSettingElipse = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // childFrmSettingElipse
@@ -40,11 +41,21 @@ namespace QLBH_BunifuUI.form
             this.childFrmSettingElipse.ElipseRadius = 0;
             this.childFrmSettingElipse.TargetControl = this;
             // 
-            // childFrmSetting
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(264, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(222, 224);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // ChildFrmSetting
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(830, 500);
+            this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "ChildFrmSetting";
             this.Text = "childFrmSetting";
@@ -55,5 +66,6 @@ namespace QLBH_BunifuUI.form
         #endregion
 
         private BunifuElipse childFrmSettingElipse;
+        private System.Windows.Forms.Button button1;
     }
 }
